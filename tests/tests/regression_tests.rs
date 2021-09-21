1da6ede8cea41332507dde5fa136e9fd2d344269
David Wendt <dcrkid@yahoo.com>
2021-09-21 17:13:45 -0400

    tests: Add a test for all core classes' instance-of relationships

--- tests/tests/regression_tests.rs
+++ tests/tests/regression_tests.rs
@@ -675,6 +675,7 @@ swf_tests! {
     (as3_bitmapdata_constr, "avm2/bitmapdata_constr", 1),
     (as3_bitmap_constr, "avm2/bitmap_constr", 1),
     #[ignore] (as3_bitmap_properties, "avm2/bitmap_properties", 1),
+    (as3_class_is, "avm2/class_is", 1),
 }
 
 // TODO: These tests have some inaccuracies currently, so we use approx_eq to test that numeric values are close enough.
